f7780645bd9d1529cb74796fda36fe79b078b28c
Linuxinet <rvkyadav71@gmail.com>
2023-12-10 08:28:47 +0530

    added meta tags, blog templates and changes

---
+++ assets/js/blog-template.js
@@ -0,0 +1,54 @@
+function createMetaTags() {
+    let head = document.head;
+    let title = document.createElement('title');
+    title.textContent = 'Blog Post Title - Vinay Kumar\'s Blog'; // Modify as needed
+
+    let metaDescription = document.createElement('meta');
+    metaDescription.name = 'description';
+    metaDescription.content = 'Detailed summary of the blog post topic.';
+
+    let metaKeywords = document.createElement('meta');
+    metaKeywords.name = 'keywords';
+    metaKeywords.content = 'Specific keywords related to the blog post, Technology, AI, Security Research';
+
+    head.appendChild(title);
+    head.appendChild(metaDescription);
+    head.appendChild(metaKeywords);
+}
+
+function createHeader() {
+    let headerHTML = `
+        <header>
+            <nav id="navbar">
+                <a href="/index.html">Home</a>
+                <a href="/blog.html">Blog</a>
+                <a href="/gallery.html">AI Art Gallery</a>
+                <a href="/projects.html">Projects</a>
+                <a href="/about.html">About Me</a>
+            </nav>
+        </header>`;
+    document.body.insertAdjacentHTML('afterbegin', headerHTML);
+}
+
+function createFooter() {
+    let footerHTML = `
+        <footer>
+            <p>© 2023 Vinay Kumar. All Rights Reserved.</p>
+        </footer>`;
+    document.body.insertAdjacentHTML('beforeend', footerHTML);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    createMetaTags();
+    createHeader();
+    createFooter();
+    // Load CSS
+    loadCSS();
+});
+
+function loadCSS() {
+    let link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = '/assets/css/style.css'; // Update with the path to your CSS file
+    document.head.appendChild(link);
+}
--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,9 +1,33 @@
-document.querySelectorAll('.short-description').forEach(element => {
-    let maxLength = 100; // Maximum number of characters
-    let trimmedString = element.textContent.substr(0, maxLength);
+function truncateDescription(description, maxLength) {
+    if (description.length > maxLength) {
+        return description.substring(0, maxLength) + '...';
+    }
+    return description;
+}
 
-    // Re-trim if we are in the middle of a word
-    trimmedString = trimmedString.substr(0, Math.min(trimmedString.length, trimmedString.lastIndexOf(" ")));
+function loadBlogPosts() {
+    fetch('/assets/blog-posts.json')
+        .then(response => response.json())
+        .then(posts => {
+            const sortedPosts = posts.sort((a, b) => new Date(b.date) - new Date(a.date));
+            const blogContainer = document.getElementById('blog');
 
-    element.textContent = trimmedString + '...';
-});
+            sortedPosts.forEach(post => {
+                const postElement = document.createElement('section');
+                postElement.className = 'blog-post';
+                postElement.innerHTML = `
+                    <img src="${post.coverImage}" alt="Cover Image for ${post.title}" class="cover-image">
+                    <div class="post-info">
+                        <h2>${post.title}</h2>
+                        <p class="post-date">${post.date}</p>
+                        <p class="short-description">${truncateDescription(post.shortDescription, 100)}</p>
+                        <div class="tags">${post.tags.map(tag => `<span>${tag}</span>`).join('')}</div>
+                        <a href="${post.link}">Read More</a>
+                    </div>`;
+                blogContainer.appendChild(postElement);
+            });
+        })
+        .catch(error => console.error('Error loading blog posts:', error));
+}
+
+document.addEventListener('DOMContentLoaded', loadBlogPosts);
